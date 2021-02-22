59ff5ae5beb0ac9659472b17725feb469b37e1f3
loushark <lou.m.reade@gmail.com>
2021-02-22 14:54:39 +0000

    passed tests for session 1

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,20 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  new_str = ""
+  string.split("").each_with_index do |letter, index|
+    if return_odds
+      index.odd? ? new_str << string[letter] : nil
+    elsif !return_odds
+      index.even? ? new_str << string[letter] : nil
+    end
+  end
+  new_str
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  new_str = ""
+  string.each_char.with_index do |c, i|
+    if c == "r" || c == "R"
+      new_str << string[i + 1]
+    end
+  end
+  new_str
 end
