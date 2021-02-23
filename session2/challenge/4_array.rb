c6e63ddc5152c27893ce6f6d0f5c2b3e3162a62e
loushark <lou.m.reade@gmail.com>
2021-02-23 17:29:16 +0000

    solved 4_array.rb challenge

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,12 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+def get_squares(numbers)
+  num =[]
+  numbers.sort.each do |n|
+    if numbers.include? n*n
+      num << n
+    end
+  end
+  num
+end
